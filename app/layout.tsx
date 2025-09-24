530165f575327c55ee97e3b1926fa3eddd9816ae
Milk <tientd4@mbbank.com.vn>
2025-09-24 16:27:59 +0700

    add redirect api

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 export async function generateMetadata(): Promise<Metadata> {
       let pageSettings = {
         title: '',
-        image: 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=1200&h=100&fit=crop&crop=center'
+        image: 'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=1200&h=1000&fit=crop&crop=center'
       }
 
   try {
