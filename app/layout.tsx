66e73ab417081ebac28836afe7f397a294ba24b1
Milk <tientd4@mbbank.com.vn>
2025-09-24 16:56:48 +0700

    add redirect api

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,8 +45,8 @@ export async function generateMetadata(): Promise<Metadata> {
       images: [
         {
           url: pageSettings.image,
-          width: 1200,
-          height: 1000,
+          width: 900,
+          height: 1200,
           alt: 'Hình ảnh demo',
         },
       ],
