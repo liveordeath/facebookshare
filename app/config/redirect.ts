123725f72b4b4ae65ed3e67568a12964590102f6
Milk <tientd4@mbbank.com.vn>
2025-09-25 14:27:21 +0700

    fix link

--- app/config/redirect.ts
+++ app/config/redirect.ts
@@ -28,15 +28,56 @@ export const urlList = [
   },
   {
     id: 2,
-    url: 'https://s.shopee.vn/8zw3h5XsET',
+    url: 'https://s.shopee.vn/VxXDJNYWZ',
     active: true
   },
   {
     id: 3,
-    url: 'https://s.shopee.vn/8zw3h5XsET', 
+    url: 'https://s.shopee.vn/qaNbxKec0', 
     active: true
-  }
+  },
+  {
+    id: 4,
+    url: 'https://s.shopee.vn/9fBm8DHphG', 
+    active: true
+  },
+  {
+    id: 5,
+    url: 'https://s.shopee.vn/9pVCKeDxzy', 
+    active: true
+  },
+  {
+    id: 6,
+    url: 'https://s.shopee.vn/1qSuo1G8q2', 
+    active: true
+  },
+  {
+    id: 7,
+    url: 'https://s.shopee.vn/8zw5L86u99', 
+    active: true
+  },
+  {
+    id: 8,
+    url: 'https://s.shopee.vn/gGxPvjq1q', 
+    active: true
+  },
+  {
+    id: 9,
+    url: 'https://s.shopee.vn/AKRSvi02aZ', 
+    active: true
+  },
+  {
+    id: 10,
+    url: 'https://s.shopee.vn/3qDzBoeU9Z', 
+    active: true
+  },
+  {
+    id: 11,
+    url: 'https://s.shopee.vn/1qSuo9uhWA', 
+    active: true
+  },
   
+
 ]
 
 // Hàm lấy URL ngẫu nhiên từ danh sách
